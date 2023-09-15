e0977ad92b0f686e2653b9f04c3dd55f5a43d264
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-15 11:19:27 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-def calculate():
+def to_uppercase(words):
     """
-    calculates the total
-    :return: None
+    Convert each word in the list to uppercase using the map function.
+    Args:
+    - words (list): List of words to be converted to uppercase.
+
+    Returns:
+    - list: List of words in uppercase.
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    # Your code here
+    return uppercase_list
 
 
 if __name__ == '__main__':
-    calculate()
+    words = ['apple', 'banana', 'cherry']
+    uppercase_list = to_uppercase(words)
+    print(uppercase_list)
--- main_test.py
+++ main_test.py
@@ -1,6 +1,7 @@
-import main
+from main import to_uppercase
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_to_uppercase():
+    assert to_uppercase(['apple', 'banana', 'cherry']) == ['APPLE', 'BANANA', 'CHERRY']
+    assert to_uppercase([]) == []  # Testen einer leeren Liste
+    assert to_uppercase(['Hello', 'World']) == ['HELLO', 'WORLD']
+    assert to_uppercase(['test', 'TEST', 'TeSt']) == ['TEST', 'TEST', 'TEST']  # Testen von gemischten Groß- und Kleinschreibungen
