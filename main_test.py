1a000fa6adf5bd42d71f2f828bacb3aaf6d6c8bf
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:05:12 +0200

    Ran pyink on branch solution

--- main_test.py
+++ main_test.py
@@ -1,7 +1,12 @@
 from main import to_uppercase
 
+
 def test_to_uppercase():
     assert to_uppercase(['apple', 'banana', 'cherry']) == ['APPLE', 'BANANA', 'CHERRY']
     assert to_uppercase([]) == []  # Testen einer leeren Liste
     assert to_uppercase(['Hello', 'World']) == ['HELLO', 'WORLD']
-    assert to_uppercase(['test', 'TEST', 'TeSt']) == ['TEST', 'TEST', 'TEST']  # Testen von gemischten Groß- und Kleinschreibungen
+    assert to_uppercase(['test', 'TEST', 'TeSt']) == [
+        'TEST',
+        'TEST',
+        'TEST',
+    ]  # Testen von gemischten Groß- und Kleinschreibungen
